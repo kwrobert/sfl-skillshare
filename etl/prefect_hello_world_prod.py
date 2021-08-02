c888c051b2128e8569c3b5b070e4c267a13ce052
Kyle Robertson <kyle.wesley@me.com>
2021-08-02 08:11:35 -0400

    Fixing flow run bug

--- etl/prefect_hello_world_prod.py
+++ etl/prefect_hello_world_prod.py
@@ -34,33 +34,12 @@ def load(users):
     logger.info(f"Hello from load! I was given {users}. I will now put it elsewhere")
 
 
-def define_pipeline():
 
-    with Flow("sfl-hello-world-prod") as flow:
-        data = extract()
-        transformed_data = transform(data)
-        load(transformed_data)
+with Flow("sfl-hello-world-prod") as flow:
+    data = extract()
+    transformed_data = transform(data)
+    load(transformed_data)
 
-    # Register the flow under the "sfl" project
-    flow.run_config = UniversalRun(labels=["sfl"])
-    flow.storage = github_storage
-    flow_id = flow.register(
-        project_name="sfl", labels=["sfl"], add_default_labels=False
-    )
-    return flow, flow_id
-    # return flow, None
-
-
-def main():
-    flow, flow_id = define_pipeline()
-    print(dir(flow))
-    print("flow_id = {}".format(flow_id))
-    # This schedules the flow to run using an Agent and Executor
-    client = Client()
-    client.create_flow_run(
-        flow_id=flow_id,
-    )
-
-
-if __name__ == "__main__":
-    main()
+# Register the flow under the "sfl" project
+flow.run_config = UniversalRun(labels=["sfl"])
+flow.storage = github_storage
